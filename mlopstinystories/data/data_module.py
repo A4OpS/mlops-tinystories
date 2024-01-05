6b23ccc2431304ad41ac9ef2d3484ad19c1226b6
Albert Garde <albertsgarde@gmail.com>
2024-01-06 00:43:51 +0100

    :card_file_box: Add script to prepare data

--- mlopstinystories/data/data_module.py
+++ mlopstinystories/data/data_module.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 import datasets
 import pandas as pd
@@ -8,13 +8,18 @@ from datasets import DatasetDict
 from pytorch_lightning import LightningDataModule
 from torch import Tensor
 from torch.utils.data import DataLoader, TensorDataset
+from transformers import PreTrainedTokenizerFast
 
+TOTAL_RATIO = 0.05
 VALIDATION_RATIO = 0.1
 TEST_RATIO = 0.05
+TOKENIZATION_BATCH_SIZE = 64
+MAX_LENGTH_TEXT = 1024
+MAX_LENGTH = 256
 
 
 class TinyStories(LightningDataModule):
-    def __init__(self, data_dir: str, device: torch.device) -> None:
+    def __init__(self, data_dir: str, tokenizer: PreTrainedTokenizerFast, device: torch.device) -> None:
         super().__init__()
         self.data_dir = data_dir
         self.raw_dir = os.path.join(data_dir, "raw")
@@ -26,10 +31,15 @@ class TinyStories(LightningDataModule):
         self.processed_dir = os.path.join(data_dir, "processed")
         if not os.path.exists(self.processed_dir):
             os.makedirs(self.processed_dir)
+        self.tokenizer = tokenizer
         self.device = device
 
-        self.train_set: Optional[TensorDataset] = None
-        self.test_set: Optional[TensorDataset] = None
+        self.train_texts: Optional[List[str]] = None
+        self.validation_texts: Optional[List[str]] = None
+        self.test_texts: Optional[List[str]] = None
+        self.train_tokens: Optional[Tensor] = None
+        self.validation_tokens: Optional[Tensor] = None
+        self.test_tokens: Optional[Tensor] = None
 
     def prepare_data(self) -> None:
         # Load data from disk.
@@ -38,41 +48,70 @@ class TinyStories(LightningDataModule):
             raise Exception("Raw data not found. Please place it in '[data_dir]/raw'.")
         raw_data: DatasetDict = datasets.load_from_disk(raw_data_path)  # type: ignore
         # Convert dataset splits to pandas dataframes.
-        train_data = raw_data["train"].data.to_pandas()
-        validation_data = raw_data["validation"].data.to_pandas()
+        train_texts = raw_data["train"].data.to_pandas()
+        validation_texts = raw_data["validation"].data.to_pandas()
         # Concatenate the dataframes.
-        data = pd.concat([train_data, validation_data])
+        data = pd.concat([train_texts, validation_texts])
 
-        shuffled_data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+        # Shuffle the data and reset the index.
+        short = data[data["text"].apply(len) <= MAX_LENGTH_TEXT]
+        short_ratio = len(short) / len(data)
+        shuffled_data = short.sample(frac=TOTAL_RATIO / short_ratio, random_state=42).reset_index(drop=True)
 
+        strings = shuffled_data["text"].tolist()
+
+        tokenized_strings = self.tokenizer(
+            strings,
+            add_special_tokens=True,
+            padding="max_length",
+            max_length=MAX_LENGTH,
+            truncation=True,
+            return_tensors="pt",
+        )
+        tokens: Tensor = tokenized_strings["input_ids"].to(torch.int32)  # type: ignore
+
+        # Split the data into train, validation and test sets.
         test_end_index = int(len(shuffled_data) * TEST_RATIO)
         validation_end_index = int(len(shuffled_data) * (TEST_RATIO + VALIDATION_RATIO))
 
-        test_data = shuffled_data[0:test_end_index]
-        validation_data = shuffled_data[test_end_index:validation_end_index]
-        train_data = shuffled_data[validation_end_index:]
-        # Split the data into train, validation and test sets.
+        test_texts = shuffled_data[0:test_end_index]
+        validation_texts = shuffled_data[test_end_index:validation_end_index]
+        train_texts = shuffled_data[validation_end_index:]
+
+        test_tokens = tokens[0:test_end_index]
+        validation_tokens = tokens[test_end_index:validation_end_index]
+        train_tokens = tokens[validation_end_index:]
 
         # Save the data to disk.
         os.makedirs(self.processed_dir, exist_ok=True)
-        train_data.to_pickle(os.path.join(self.processed_dir, "train_data.pkl"))
-        validation_data.to_pickle(os.path.join(self.processed_dir, "validation_data.pkl"))
-        test_data.to_pickle(os.path.join(self.processed_dir, "test_data.pkl"))
+        train_texts.to_pickle(os.path.join(self.processed_dir, "train_texts.pkl"))
+        validation_texts.to_pickle(os.path.join(self.processed_dir, "validation_texts.pkl"))
+        test_texts.to_pickle(os.path.join(self.processed_dir, "test_texts.pkl"))
+
+        torch.save(train_tokens.clone(), os.path.join(self.processed_dir, "train_tokens.pt"))
+        torch.save(validation_tokens.clone(), os.path.join(self.processed_dir, "validation_tokens.pt"))
+        torch.save(test_tokens.clone(), os.path.join(self.processed_dir, "test_tokens.pt"))
 
     def setup(self, stage: str) -> None:
         # Load data from disk
-        self.train_data = pd.read_pickle(os.path.join(self.processed_dir, "train_data.pkl"))
-        self.validation_data = pd.read_pickle(os.path.join(self.processed_dir, "validation_data.pkl"))
-        self.test_data = pd.read_pickle(os.path.join(self.processed_dir, "test_data.pkl"))
+        self.train_texts = pd.read_pickle(os.path.join(self.processed_dir, "train_texts.pkl"))["texts"].tolist()
+        self.validation_texts = pd.read_pickle(os.path.join(self.processed_dir, "validation_texts.pkl")).tolist()
+        self.test_texts = pd.read_pickle(os.path.join(self.processed_dir, "test_texts.pkl")).tolist()
 
-        return
+        self.train_tokens = torch.load(os.path.join(self.processed_dir, "train_tokens.pt"))
+        self.validation_tokens = torch.load(os.path.join(self.processed_dir, "validation_tokens.pt"))
+        self.test_tokens = torch.load(os.path.join(self.processed_dir, "test_tokens.pt"))
 
-    def train_dataloader(self) -> DataLoader[List[Tensor]]:
-        if self.train_set is None:
+        self.train_set = TensorDataset(self.train_tokens)
+        self.validation_set = TensorDataset(self.validation_tokens)
+        self.test_set = TensorDataset(self.test_tokens)
+
+    def train_dataloader(self) -> DataLoader[Tensor]:
+        if self.train_texts is None or self.train_tokens is None:
             raise Exception("Please call setup() before using this dataloader.")
         pin_memory = self.device != torch.device("cpu")
         if pin_memory:
-            train_loader: DataLoader[List[Tensor]] = DataLoader(  # type: ignore
+            train_loader: DataLoader[Tensor] = DataLoader(  # type: ignore
                 self.train_set,
                 batch_size=64,
                 shuffle=True,
@@ -90,12 +129,32 @@ class TinyStories(LightningDataModule):
 
         return train_loader
 
-    def test_dataloader(self) -> DataLoader[List[Tensor]]:
-        if self.test_set is None:
+    def val_dataloader(self) -> DataLoader[Tensor]:
+        if self.validation_set is None or self.validation_tokens is None:
             raise Exception("Please call setup() before using this dataloader.")
         pin_memory = self.device != torch.device("cpu")
         if pin_memory:
-            test_loader: DataLoader[List[Tensor]] = DataLoader(  # type: ignore
+            validation_loader: DataLoader[Tensor] = DataLoader(  # type: ignore
+                self.validation_set,
+                batch_size=64,
+                num_workers=torch.get_num_threads(),
+                pin_memory=True,
+                pin_memory_device=str(self.device),
+            )
+        else:
+            validation_loader = DataLoader(  # type: ignore
+                self.validation_set,
+                batch_size=64,
+                num_workers=torch.get_num_threads(),
+            )
+        return validation_loader
+
+    def test_dataloader(self) -> DataLoader[Tensor]:
+        if self.test_set is None or self.test_tokens is None:
+            raise Exception("Please call setup() before using this dataloader.")
+        pin_memory = self.device != torch.device("cpu")
+        if pin_memory:
+            test_loader: DataLoader[Tensor] = DataLoader(  # type: ignore
                 self.test_set,
                 batch_size=64,
                 num_workers=torch.get_num_threads(),
--- mlopstinystories/data/process_data.py
+++ mlopstinystories/data/process_data.py
@@ -1,11 +1,18 @@
 import logging
+from datetime import datetime
 
 import torch
 from data_module import TinyStories
+from transformers import AutoTokenizer, PreTrainedTokenizerFast
 
 if __name__ == "__main__":
     log = logging.getLogger(__name__)
 
-    # Get dataset from huggingface
-    data = TinyStories("data", torch.device("cpu"))
+    tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")  # type: ignore
+    tokenizer.pad_token = tokenizer.eos_token
+
+    start_time = datetime.now()
+    data = TinyStories("data", tokenizer, torch.device("cpu"))
     data.prepare_data()
+    end_time = datetime.now()
+    print(f"Time to prepare data: {end_time - start_time}")
