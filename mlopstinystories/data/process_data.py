dd99aaebee4e828f78e7e1b7c89e70ea8368d88a
Albert Garde <albertsgarde@gmail.com>
2024-01-10 16:43:50 +0100

    :recycle: Move code to `main` function

--- mlopstinystories/data/process_data.py
+++ mlopstinystories/data/process_data.py
@@ -1,15 +1,17 @@
-import logging
 from datetime import datetime
 
 import torch
 from data_module import TinyStories, TinyStoriesConfig
 
-if __name__ == "__main__":
-    log = logging.getLogger(__name__)
 
+def main():
     start_time = datetime.now()
     config = TinyStoriesConfig()
     data = TinyStories("data", torch.device("cpu"), config)
     data.prepare_data()
     end_time = datetime.now()
     print(f"Time to prepare data: {end_time - start_time}")
+
+
+if __name__ == "__main__":
+    main()
