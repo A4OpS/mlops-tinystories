140f2fae274cdf8968f01516dca43ca2c763b71f
Albert Garde <albertsgarde@gmail.com>
2024-01-10 13:51:16 +0100

    :sparkles: Create basic script to train model

--- mlopstinystories/train_model.py
+++ mlopstinystories/train_model.py
@@ -1,5 +1,6 @@
 from device import get_device
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
 from data import TinyStories
@@ -23,8 +24,24 @@ if __name__ == "__main__":
     print("Device: ", model.device())
     print(f"Number of parameters: {model.num_params()}")
 
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="models/checkpoints",
+        save_top_k=3,
+        save_on_train_epoch_end=False,
+        monitor="val_loss",
+        filename="{step}-{val_loss:.2f}",
+        mode="min",
+    )
     trainer = Trainer(
-        logger=WandbLogger(project="mlops-tinystories"), accelerator=device.lightning(), max_epochs=1, max_steps=10
+        logger=WandbLogger(project="mlops-tinystories"),
+        accelerator=device.lightning(),
+        max_epochs=1,
+        callbacks=[checkpoint_callback],
+        max_steps=50,
+        val_check_interval=5,
+        limit_val_batches=10,
+        log_every_n_steps=1,
+        num_sanity_val_steps=0,
     )
 
     trainer.fit(model, datamodule=data)
