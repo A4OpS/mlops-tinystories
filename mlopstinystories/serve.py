3884c4cd4cb1a7f11ee60ffd792505747663b5a4
Albert Garde <albertsgarde@gmail.com>
2024-01-11 20:21:30 +0100

    :zap: Cache models in server

--- mlopstinystories/serve.py
+++ mlopstinystories/serve.py
@@ -1,21 +1,58 @@
-from enum import Enum
+from contextlib import asynccontextmanager
 from http import HTTPStatus
+from typing import Dict, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from transformers import PreTrainedTokenizerFast
 from transformers.generation import GenerationConfig
 
 from .data import TinyStories
-from .device import get_device
-from .models import TinyStoriesModel
+from .device import Device, get_device
+from .models import ModelNotFoundError, TinyStoriesModel
 
 
-class ItemEnum(Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+class AppState:
+    _device: Device
+    _tokenizer: PreTrainedTokenizerFast
+    _models: Dict[str, TinyStoriesModel]
+
+    def __init__(
+        self,
+        device: Device,
+        tokenizer: PreTrainedTokenizerFast,
+    ):
+        self._device = device
+        self._tokenizer = tokenizer
+        self._models = {}
+
+    @property
+    def device(self) -> Device:
+        return self._device
+
+    @property
+    def tokenizer(self) -> PreTrainedTokenizerFast:
+        return self._tokenizer
+
+    def get_model(self, model_path: str) -> Optional[TinyStoriesModel]:
+        if model_path not in self._models:
+            try:
+                model = TinyStoriesModel.load(model_path, self.device.torch())
+                self._models[model_path] = model
+            except ModelNotFoundError:
+                return None
+
+        return self._models[model_path]
 
 
-app = FastAPI()
+@asynccontextmanager
+async def initialize(app: FastAPI):
+    device = get_device()
+    tokenizer = TinyStories.create_tokenizer()
+    app.state.state = AppState(device, tokenizer)
+    yield
+
+
+app = FastAPI(lifespan=initialize)
 
 
 @app.get("/")
@@ -28,13 +65,21 @@ def read_root():
     return response
 
 
-@app.get("/generate/{model_name}")
-def read_item(model_name: str, input_text: str):
-    device = get_device()
+@app.get("/generate/{model_path}")
+def generate(model_path: str, input_text: str, request: Request):
+    state: AppState = request.app.state.state
 
-    tokenizer = TinyStories.create_tokenizer()
+    device = state.device
 
-    model = TinyStoriesModel.load(model_name, device.torch())
+    tokenizer = state.tokenizer
+
+    model = state.get_model(model_path)
+    if model is None:
+        response = {
+            "message": f"Model '{model_path}' not found",
+            "status-code": HTTPStatus.NOT_FOUND,
+        }
+        return response
 
     input_tokens = tokenizer(input_text, return_tensors="pt").input_ids
 
