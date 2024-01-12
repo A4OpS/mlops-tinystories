da70143332a6db5700af01c186581f4e63752040
Aleksander Svendstorp <s194323@student.dtu.dk>
2024-01-12 12:13:15 +0100

    Merge main

--- mlopstinystories/profiler.py
+++ mlopstinystories/profiler.py
@@ -1,5 +1,8 @@
+import hydra
+from hydra.core.config_store import ConfigStore
 from pytorch_lightning.profilers import PyTorchProfiler
 from torch.profiler import ProfilerActivity, tensorboard_trace_handler
+from train_model import TrainModelConfig, train_model
 
 profiler = PyTorchProfiler(
         activities=[ProfilerActivity.CPU],
@@ -10,3 +13,14 @@ profiler = PyTorchProfiler(
         sort_by_key = "cpu_time_total",
         on_trace_ready = tensorboard_trace_handler("./profilers")
     )
+
+cs = ConfigStore.instance()
+
+cs.store(name="train_config", node=TrainModelConfig)
+
+@hydra.main(config_path="../conf/train", version_base="1.3")
+def main(config: TrainModelConfig) -> None:
+    train_model(config, profiler)
+
+if __name__ == "__main__":
+    main()
