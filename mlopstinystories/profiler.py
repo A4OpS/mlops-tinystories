045b640f99ade289406bf60096c0e96ba847b95b
Albert Garde <albertsgarde@gmail.com>
2024-01-12 14:10:23 +0100

    :sparkles: Add CUDA activity to profiler

--- mlopstinystories/profiler.py
+++ mlopstinystories/profiler.py
@@ -4,23 +4,25 @@ from pytorch_lightning.profilers import PyTorchProfiler
 from torch.profiler import ProfilerActivity, tensorboard_trace_handler
 from train_model import TrainModelConfig, train_model
 
-profiler = PyTorchProfiler(
-        activities=[ProfilerActivity.CPU],
-        row_limit = 20,
-        record_shapes = True,
-        with_stack = True,
-        profile_memory = True,
-        sort_by_key = "cpu_time_total",
-        on_trace_ready = tensorboard_trace_handler("./profilers")
-    )
-
 cs = ConfigStore.instance()
 
 cs.store(name="train_config", node=TrainModelConfig)
 
+
 @hydra.main(config_path="../conf/train", version_base="1.3")
 def main(config: TrainModelConfig) -> None:
+    profiler = PyTorchProfiler(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        row_limit=20,
+        record_shapes=True,
+        with_stack=True,
+        profile_memory=True,
+        sort_by_key="cpu_time_total",
+        on_trace_ready=tensorboard_trace_handler("./profilers"),
+    )
+
     train_model(config, profiler)
+
 
 if __name__ == "__main__":
     main()
