befa0be5ba84416205542416c77858792436add1
AndreasRaaskov <s183901@student.dtu.dk>
2024-01-17 10:15:58 +0100

    Imporved debugging
    I commented out the deploy container (until we need it) also removed the entry point because interactive containers are so much better for debugging, just remeber to run containers with the flag -it

--- dockerfiles/BuildPush.bash
+++ dockerfiles/BuildPush.bash
@@ -10,7 +10,7 @@ set -e
 # Define variables
 
 #The name of the image that will be built / the name of the docker repository
-IMAGE_NAME="mlops-tinystories"
+IMAGE_NAME="mlops-tinystories-cuda"
 
 #The base container holde all dependencies for the and is used to build specific containers 
 #base.dockerfile is the dockerfile for the base container and base_cuda.dockerfile is the dockerfile for the cuda base container
@@ -65,16 +65,15 @@ docker build --file dockerfiles/$base_container --tag base . \
 echo "Base container built"
 
 #Build the deploy container
-docker build --file dockerfiles/deploy.dockerfile --build-arg BASE_IMAGE=base  \
-    --tag docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$deploy_TAG .
+#docker build --file dockerfiles/deploy.dockerfile --build-arg BASE_IMAGE=base  \
+#    --tag docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$deploy_TAG .
 
-echo "Deploy container built"
+#echo "Deploy container built"
 
-if [ $push == "y" ]; then
+#if [ $push == "y" ]; then
     #push the deploy container
-    docker push docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$deploy_TAG
-fi
-
+#    docker push docker.io/$DOCKER_USERNAME/$IMAGE_NAME:$deploy_TAG
+#fi
 
 
 #Build the train container
