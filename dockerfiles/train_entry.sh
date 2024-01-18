6fc1908e072f49e20f92573e9eb04e4cf6d5600d
SentientSchnitzel <simondanielschneider@gmail.com>
2024-01-18 15:33:27 +0100

    spliting into specific entry scripts

--- dockerfiles/train_entry.sh
+++ dockerfiles/train_entry.sh
@@ -6,3 +6,6 @@ echo "Finished pulling data"
 
 # Execute the main command that is passed to this entrypoint
 exec "$@"
+
+# dvc push
+# this training technically has to push the generated models to the remote bucket as well. #TODO
