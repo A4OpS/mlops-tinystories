17b69851cd8b64e59a59cf0e062a6d11f21e7b7c
Albert Garde <albertsgarde@gmail.com>
2024-01-18 12:01:06 +0100

    :fire: Remove problematic files from `__init__.py`

--- mlopstinystories/__init__.py
+++ mlopstinystories/__init__.py
@@ -1,5 +1,1 @@
 """Replication of the Tiny Stories paper."""
-from .train_model import TrainModelConfig
-from .train_model import main as train_model
-
-__all__ = ["train_model", "TrainModelConfig"]
